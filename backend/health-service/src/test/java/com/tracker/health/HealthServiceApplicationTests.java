e3d4da727a6dad5edd4c9ff5eb39a442bc3af289
akash5.yadav <akash5.yadav@paytm.com>
2026-04-02 11:04:15 +0530

    feat: bootstrap health-service project structure

---
+++ backend/health-service/src/test/java/com/tracker/health/HealthServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.tracker.health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
